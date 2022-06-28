289dc3cfe88db92d39e2d4d0587f51659da1efde
2alheure <jojo77.juv@live.fr>
2022-06-28 23:05:20 +0200

    Token OK

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Token;
 use App\Models\Question;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Builder;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class QuestionController extends Controller {
 
     const DEFAULT_LIMIT = 10;
 
     public function get(Request $request) {
+        if (!empty($token = $request->token)) {
+            $token = Token::where('token', $token)->first();
+            if (empty($token)) throw new BadRequestHttpException('Token invalide.');
+        }
+
         $qb = Question::with(['category', 'language', 'responses', 'type'])
             ->whereNull('deleted_at');
 
@@ -20,7 +28,23 @@ class QuestionController extends Controller {
         if (!empty($language = $request->language)) $qb->whereHas('language', function (Builder $qb) use ($language) {
             $qb->where('abbr', $language);
         });
+        if (!empty($token)) $qb->whereDoesntHave('sessions', function (Builder $query) use ($token) {
+            $query->where('token', $token->token);
+        });
 
-        return new JsonResponse($qb->simplePaginate($request->limit ?? static::DEFAULT_LIMIT));
+        $questions = $qb->inRandomOrder()->limit($request->limit ?? static::DEFAULT_LIMIT)->get();
+        if (!empty($token)) {
+            $array = $questions->map(function ($question) use ($token) {
+                return [
+                    'token_id' => $token->id,
+                    'question_id' => $question->id
+                ];
+            })
+                ->toArray();
+
+            DB::table('session')->insert($array);
+        }
+
+        return new JsonResponse($questions);
     }
 }
