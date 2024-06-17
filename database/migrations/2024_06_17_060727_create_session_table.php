b284a8d8c0728448ab29d8cf48ac50851c2dc4d7
2alheure <jojo77.juv@live.fr>
2024-06-17 08:20:44 +0200

    db prefix + migrations

---
+++ database/migrations/2024_06_17_060727_create_session_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('session', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('token_id');
+            $table->integer('question_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('token_id')->references('id')->on('token');
+            $table->foreign('question_id')->references('id')->on('question');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('session');
+    }
+};
